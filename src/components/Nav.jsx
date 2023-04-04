bdaf4e069d5860374e950765db05458532372003
TomassReynoso <tomasreynoso17@gmail.com>
2023-04-04 09:24:29 -0300

    Proyect

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import SearchBar from './components/SearchBar.jsx';
+
+function Nav(SearchBar) {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
+}
+
+export default Nav
